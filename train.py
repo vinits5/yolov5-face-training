d824e95b495e4000996e7b2be43b900475024fef
OEM Configuration (temporary user) <vinitsarode5@gmail.com>
2021-11-03 13:50:45 +0530

    debugging the test setup

--- train.py
+++ train.py
@@ -264,6 +264,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
             pbar = tqdm(pbar, total=nb)  # progress bar
         optimizer.zero_grad()
         for i, (imgs, targets, paths, _) in pbar:  # batch -------------------------------------------------------------
+            if i>2: break
             ni = i + nb * epoch  # number integrated batches (since train start)
             imgs = imgs.to(device, non_blocking=True).float() / 255.0  # uint8 to float32, 0-255 to 0.0-1.0
 
