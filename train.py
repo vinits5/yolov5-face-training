d6a0ef64e6f840d1bfac23dcc741a8e6a4a53923
OEM Configuration (temporary user) <vinitsarode5@gmail.com>
2021-12-06 20:46:24 +0530

    yolo v5n -> yolo v5n6

--- train.py
+++ train.py
@@ -433,7 +433,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='models/yolov5n.yaml', help='model.yaml path')
+    parser.add_argument('--cfg', type=str, default='models/yolov5n6.yaml', help='model.yaml path')
     parser.add_argument('--data', type=str, default='data/widerface.yaml', help='data.yaml path')
     parser.add_argument('--hyp', type=str, default='data/hyp.scratch.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=250)
